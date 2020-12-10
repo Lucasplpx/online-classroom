47347b1af5a05990cdc4538d49ab9301b05c040e
Lucas Passos <lucasplpx@gmail.com>
2020-12-10 20:44:47 -0300

    Ajustes comunicao front e back

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,10 +1,12 @@
-import { useCallback, useState } from 'react';
+import { useState, useCallback } from 'react';
+import useSWR from 'swr';
 import { NextPage } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
-import { signIn, signOut, useSession } from 'next-auth/client';
 
-import Nav from '../../components/nav';
 import api from '../../utils/api';
+import Nav from '../../components/nav';
+import Footer from '../../components/footer';
 
 interface Teacher {
   _id: string;
@@ -15,68 +17,70 @@ interface Teacher {
   coins: number;
   courses: string[];
   available_hours: Record<string, number[]>;
-  available_locations: string;
-  reviews: Record<string, unknown>;
-  appointments: Record<string, unknown>;
+  available_locations: string[];
+  reviews: Record<string, unknown>[];
+  appointments: Record<string, unknown>[];
 }
 
 const SearchPage: NextPage = () => {
   const [textInput, setTextInput] = useState('');
-  const [data, setData] = useState<Teacher[]>([]);
-  const [session, loading] = useSession();
 
-  const handleSearch = useCallback(() => {
-    api(`/api/search/${textInput}`).then((response) => {
-      const teachers: Teacher[] = response.data;
-      setData(teachers);
-    });
-  }, [textInput]);
+  const { data, error } = useSWR(
+    textInput !== '' ? `/api/search/${textInput}` : null,
+    api
+  );
+
+  const handleSearch = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      setTextInput(document.getElementsByTagName('input')[0].value);
+    },
+    [setTextInput]
+  );
 
   return (
     <div>
       <Nav />
 
-      {!session && (
-        <div className="text-3xl">
-          Not signed in <br />
-          <button onClick={(): Promise<void> => signIn('auth0')}>
-            Sign in
-          </button>
-        </div>
-      )}
-      {session && (
-        <>
-          <h1>Bem vindo a página SEARCH</h1>
-          <div className="text-3xl">
-            Signed in as {session?.user.email} <br />
-            <button onClick={(): Promise<void> => signOut()}>Sign out</button>
-          </div>
+      <div className="text-center">
+        <form onSubmit={handleSearch}>
           <input
             type="text"
-            value={textInput}
-            onChange={(e) => setTextInput(e.target.value)}
-            placeholder="Nome da matéria"
-            className="bg-pink-200"
+            placeholder="Digite a disciplina que você procura..."
+            className="text-2xl border-2 border-box w-3/12 m-auto text-center my-12"
           />
-          <button type="button" className="bg-blue-200" onClick={handleSearch}>
+          <button type="submit" className="hidden">
             Pesquisar
           </button>
-          {data.length !== 0 &&
-            data.map((teacher) => (
-              <Link href={`/search/${teacher._id}`} key={teacher._id}>
-                <a>
-                  <h1 className="text-2xl"> {teacher.name} </h1>
-                </a>
-              </Link>
-            ))}
-        </>
-      )}
+        </form>
+        {data &&
+          data.data.map((teacher) => (
+            <Link href={`/search/${teacher._id}`} key={teacher._id}>
+              <a>
+                <h1 className="text-2xl border-2 border-box w-1/2 m-auto mt-4 py-2">
+                  {teacher.name}
+                </h1>
+              </a>
+            </Link>
+          ))}
 
-      {loading && (
-        <div className="text-5xl">
-          <h1>Carregando...</h1>
-        </div>
-      )}
+        {error && (
+          <div className="text-xl">
+            <h1>Erro na busca pela matéria {textInput}</h1>
+          </div>
+        )}
+      </div>
+
+      <div className="text-center mb-10">
+        <Image
+          src="/logo.jpg"
+          alt="Logo Teach Other"
+          width={485}
+          height={485}
+        />
+      </div>
+      <Footer />
     </div>
   );
 };
